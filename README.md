b2f946c0d2eac1286b6e461a11af34ced1aa01e0
Dom DeStefano <domdest@gmail.com>
2022-09-07 20:55:05 -0400

    add Sonata bundle to composer and add TODO list to readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # orgyourway
 A toolset of simple site functions that empower the creation of small community organization sites.
+
+TODO:
+- pre-populate db user in .sql file during Docker build
+- add composer install to Docker build
+- user authentication
--- app/config/bundles.php
+++ app/config/bundles.php
@@ -12,4 +12,11 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Sonata\Twig\Bridge\Symfony\SonataTwigBundle::class => ['all' => true],
+    Sonata\Doctrine\Bridge\Symfony\SonataDoctrineBundle::class => ['all' => true],
+    Sonata\Form\Bridge\Symfony\SonataFormBundle::class => ['all' => true],
+    Sonata\Exporter\Bridge\Symfony\SonataExporterBundle::class => ['all' => true],
+    Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
 ];
