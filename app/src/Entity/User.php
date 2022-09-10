58e16673837051af4cdc6590271a6b9fbc2ad30c
Dom DeStefano <domdest@gmail.com>
2022-09-10 18:04:45 -0400

    create User for security component

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -2,279 +2,98 @@
 
 namespace App\Entity;
 
-use DateTime;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\Types;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\ORM\Mapping\Column;
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\Id;
-use Doctrine\ORM\Mapping\ManyToMany;
-use Doctrine\ORM\Mapping\Table;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-#[Entity]
-#[Table(name: 'users')]
-class User
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    #[Id, Column(
-        type: Types::INTEGER,
-    )]
-    private ?int $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
-    #[Column(
-        name: 'first_name',
-        type: Types::STRING,
-        nullable: true
-    )]
-    private ?string $firstName;
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
 
-    #[Column(
-        name: 'last_name',
-        type: Types::STRING,
-        nullable: true
-    )]
-    private ?string $lastName;
-
-    #[Column(
-        type: Types::STRING,
-        nullable: true
-    )]
-    private ?string $alias;
-
-    #[Column(
-        type: Types::STRING
-    )]
-    private string $email;
-
-    #[Column(
-        type: Types::BOOLEAN
-    )]
-    private bool $subscribed;
-
-    #[Column(
-        name: 'subscribed_at',
-        type: Types::DATETIME_MUTABLE,
-        nullable: true
-    )]
-    private ?DateTime $subscribedAt;
-
-    #[Column(
-        name: 'unsubscribed_at',
-        type: Types::DATETIME_MUTABLE,
-        nullable: true
-    )]
-    private ?DateTime $unsubscribedAt;
-
-    #[Column(
-        name: 'created_at',
-        type: Types::DATETIME_MUTABLE
-    )]
-    private DateTime $createdAt;
-
-    #[Column(
-        name: 'updated_at',
-        type: Types::DATETIME_MUTABLE
-    )]
-    private DateTime $updatedAt;
-
-    #[Column(
-        name: 'deleted_at',
-        type: Types::DATETIME_MUTABLE,
-        nullable: true
-    )]
-    private DateTime $deletedAt;
-
-    #[ManyToMany(targetEntity: 'Event', mappedBy: 'users')]
-    private Collection $events;
+    #[ORM\Column]
+    private array $roles = [];
 
     /**
-     * @return int|null
+     * @var string The hashed password
      */
+    #[ORM\Column]
+    private ?string $password = null;
+
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * @param int|null $id
-     */
-    public function setId(?int $id): void
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getFirstName(): ?string
-    {
-        return $this->firstName;
-    }
-
-    /**
-     * @param string|null $firstName
-     */
-    public function setFirstName(?string $firstName): void
-    {
-        $this->firstName = $firstName;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getLastName(): ?string
-    {
-        return $this->lastName;
-    }
-
-    /**
-     * @param string|null $lastName
-     */
-    public function setLastName(?string $lastName): void
-    {
-        $this->lastName = $lastName;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getAlias(): ?string
-    {
-        return $this->alias;
-    }
-
-    /**
-     * @param string|null $alias
-     */
-    public function setAlias(?string $alias): void
-    {
-        $this->alias = $alias;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEmail(): string
+    public function getEmail(): ?string
     {
         return $this->email;
     }
 
-    /**
-     * @param string $email
-     */
-    public function setEmail(string $email): void
+    public function setEmail(string $email): self
     {
         $this->email = $email;
+
+        return $this;
     }
 
     /**
-     * @return bool
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
      */
-    public function isSubscribed(): bool
+    public function getUserIdentifier(): string
     {
-        return $this->subscribed;
+        return (string) $this->email;
     }
 
     /**
-     * @param bool $subscribed
+     * @see UserInterface
      */
-    public function setSubscribed(bool $subscribed): void
+    public function getRoles(): array
     {
-        $this->subscribed = $subscribed;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
     }
 
     /**
-     * @return DateTime|null
+     * @see PasswordAuthenticatedUserInterface
      */
-    public function getSubscribedAt(): ?DateTime
+    public function getPassword(): string
     {
-        return $this->subscribedAt;
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
     }
 
     /**
-     * @param DateTime|null $subscribedAt
+     * @see UserInterface
      */
-    public function setSubscribedAt(?DateTime $subscribedAt): void
+    public function eraseCredentials()
     {
-        $this->subscribedAt = $subscribedAt;
-    }
-
-    /**
-     * @return DateTime|null
-     */
-    public function getUnsubscribedAt(): ?DateTime
-    {
-        return $this->unsubscribedAt;
-    }
-
-    /**
-     * @param DateTime|null $unsubscribedAt
-     */
-    public function setUnsubscribedAt(?DateTime $unsubscribedAt): void
-    {
-        $this->unsubscribedAt = $unsubscribedAt;
-    }
-
-    /**
-     * @ORM\PrePersist
-     * @ORM\PreUpdate
-     */
-    public function updatedTimestamps(): void
-    {
-        $dateTimeNow = new DateTime('now');
-
-        $this->setUpdatedAt($dateTimeNow);
-
-        if ($this->getCreatedAt() === null) {
-            $this->setCreatedAt($dateTimeNow);
-        }
-    }
-
-    /**
-     * @return DateTime
-     */
-    public function getCreatedAt(): DateTime
-    {
-        return $this->createdAt;
-    }
-
-    /**
-     * @param DateTime $createdAt
-     */
-    public function setCreatedAt(DateTime $createdAt): void
-    {
-        $this->createdAt = $createdAt;
-    }
-
-    /**
-     * @return DateTime
-     */
-    public function getUpdatedAt(): DateTime
-    {
-        return $this->updatedAt;
-    }
-
-    /**
-     * @param DateTime $updatedAt
-     */
-    public function setUpdatedAt(DateTime $updatedAt): void
-    {
-        $this->updatedAt = $updatedAt;
-    }
-
-    /**
-     * @return DateTime
-     */
-    public function getDeletedAt(): DateTime
-    {
-        return $this->deletedAt;
-    }
-
-    /**
-     * @param DateTime $deletedAt
-     */
-    public function setDeletedAt(DateTime $deletedAt): void
-    {
-        $this->deletedAt = $deletedAt;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
