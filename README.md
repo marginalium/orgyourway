87544f76c5c57334cd02be2df7884aac43524342
Dom DeStefano <domdest@gmail.com>
2022-09-10 20:00:03 -0400

    run Doctrine migrations on build to update schema automatically, and import backup.sql

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 A toolset of simple site functions that empower the creation of small community organization sites.
 
 TODO:
-- add composer install to Docker build
 - user authentication
--- docker/nginx/entrypoint/orgyourway-entrypoint.sh
+++ docker/nginx/entrypoint/orgyourway-entrypoint.sh
@@ -4,4 +4,7 @@ service php8.1-fpm start
 
 service nginx start
 
+/entrypoint/waitforit.sh orgyourway-mysql:3306 -t 30
+bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
+
 exec tail -f /dev/null
