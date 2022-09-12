7b4efe7b6b9bb41a1db8dbeed9b184417b69bad9
Dom DeStefano <domdest@gmail.com>
2022-09-11 21:05:55 -0400

    create UserAdmin

--- app/src/Admin/EventAdmin.php
+++ app/src/Admin/EventAdmin.php
@@ -7,26 +7,43 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class EventAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $form): void
     {
-        $form->add('name', TextType::class);
+        $form
+            ->add('event_name', TextType::class)
+            ->add(
+                'attendance_count',
+                TextType::class,
+                [
+                    'attr' => [
+                        'readonly' => true
+                    ]
+                ]
+            )
+            ->add('attendance_cap', IntegerType::class)
+            ->add('ticket_cost_in_cents', IntegerType::class)
+            ->add('started_at', DateType::class)
+            ->add('ended_at', DateType::class);
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagrid): void
     {
-        $datagrid->add('name');
+        $datagrid->add('event_name');
     }
 
     protected function configureListFields(ListMapper $list): void
     {
-        $list->addIdentifier('name');
+        $list->addIdentifier('event_name');
     }
 
     protected function configureShowFields(ShowMapper $show): void
     {
-        $show->add('name');
+        $show->add('event_name');
     }
 }
---
+++ app/src/Admin/UserAdmin.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class UserAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('email', TextType::class, ['attr' => ['readonly' => true]])
+            ->add('first_name', TextType::class)
+            ->add('last_name', TextType::class)
+            ->add('alias', TextType::class)
+            ->add('subscribed', CheckboxType::class)
+            ->add('subscribed_at', DateType::class)
+            ->add('unsubscribed_at', DateType::class);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagrid): void
+    {
+        $datagrid->add('email');
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list->addIdentifier('email');
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show->add('email');
+    }
+}
