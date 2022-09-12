065ed2b11ce83aedfde3ec9a511cd2688a63c593
Dom DeStefano <domdest@gmail.com>
2022-09-11 23:20:08 -0400

    make rocket go faster now, don't mount vendor directory and do fresh composer install on each build for faster server request times

--- docker/nginx/entrypoint/orgyourway-entrypoint.sh
+++ docker/nginx/entrypoint/orgyourway-entrypoint.sh
@@ -6,7 +6,7 @@ service php8.1-fpm start
 
 service nginx start
 
-/entrypoint/waitforit.sh orgyourway-mysql:3306 -t 30
+/entrypoint/waitforit.sh orgyourway-mysql:3306 -t 100
 bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 cp /var/www/html/.env.dist /var/www/html/.env
@@ -18,5 +18,9 @@ sed -i "s/%%MYSQL_PASSWORD%%/${MYSQL_PASSWORD}/" /var/www/html/.env
 sed -i "s/%%MYSQL_DATABASE%%/${MYSQL_DATABASE}/" /var/www/html/.env
 sed -i "s/%%ADMIN_USERNAME%%/${ADMIN_USERNAME}/" /var/www/html/.env
 sed -i "s,%%ADMIN_PASSWORD%%,$HASHED_PASSWORD," /var/www/html/.env
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+printf "${GREEN}Setup completed!${NC}"
 
 exec tail -f /dev/null
