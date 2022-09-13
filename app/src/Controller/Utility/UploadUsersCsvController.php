01dce350b6c7b623e3259560a0265853ec5e807b
Dom DeStefano <domdest@gmail.com>
2022-09-12 21:46:39 -0400

    render upload form for user csv

--- app/src/Controller/Utility/UploadUsersCsvController.php
+++ app/src/Controller/Utility/UploadUsersCsvController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Utility;
 use App\Form\UploadUsersCsvForm;
 use App\Service\UploadUsersCsvLoader;
 use App\Service\UploadUsersCsvPersister;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,17 +30,32 @@ class UploadUsersCsvController extends AbstractController
     #[Route('/upload/users/csv', name: 'app_utility_upload_users_csv')]
     public function index(Request $request): Response
     {
-        $csv_data = [];
+        $csvData = [];
         $form = $this->createForm(UploadUsersCsvForm::class);
         $form->handleRequest($request);
 
+        $preview = $form->get('preview');
+        $action = $preview->isClicked() ? 'preview' : 'submit';
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->getData()['file'];
+            $sheetData = $this->getSheetData($file);
+
+            var_dump($sheetData);
+            die();
+        }
+
         return $this->render('utility_upload_users_csv/index.html.twig', [
+            'form' => $form->createView(),
+            'generated' => [],
+            'csvData' => $csvData,
             'controller_name' => 'UploadUsersCsvController',
         ]);
     }
 
     private function getSheetData(UploadedFile $uploadedFile): array
     {
-
+        $spreadsheet = IOFactory::load($uploadedFile->getRealPath());
+        return $spreadsheet->getActiveSheet()->toArray(null, true, false);
     }
 }
