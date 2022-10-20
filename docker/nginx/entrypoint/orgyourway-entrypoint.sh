d27f5ab6de166a6989285459318d479c16bb82a9
Dominic DeStefano <domdest@gmail.com>
2022-10-19 22:24:30 -0400

    convert all sed statements in entrypoint to utilize environment variables that can be set using github actions, BOOYAH

--- docker/nginx/entrypoint/orgyourway-entrypoint.sh
+++ docker/nginx/entrypoint/orgyourway-entrypoint.sh
@@ -5,27 +5,28 @@ phpenmod gd
 phpenmod mbstring
 phpenmod zip
 
-service php8.1-fpm start
+export ADMIN_USERNAME=$ADMIN_USERNAME
+export ADMIN_PASSWORD=$ADMIN_PASSWORD
+export APP_NAME=$APP_NAME
+export MYSQL_DATABASE=$MYSQL_DATABASE
+export MYSQL_HOST=$MYSQL_HOST
+export MYSQL_USER=$MYSQL_USER
+export MYSQL_PASSWORD=$MYSQL_PASSWORD
+export HASHED_PASSWORD=`php /entrypoint/bcrypt_password_hash.php`
 
 service nginx start
+
+/etc/init.d/php8.1-fpm start
 
 /entrypoint/waitforit.sh orgyourway-mysql:3306 -t 100
 bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 cp /var/www/html/.env.dist /var/www/html/.env
 
-HASHED_PASSWORD=`php /entrypoint/bcrypt_password_hash.php`
-
-sed -i "s/%%MYSQL_USER%%/${MYSQL_USER}/" /var/www/html/.env
-sed -i "s/%%MYSQL_PASSWORD%%/${MYSQL_PASSWORD}/" /var/www/html/.env
-sed -i "s/%%MYSQL_DATABASE%%/${MYSQL_DATABASE}/" /var/www/html/.env
-sed -i "s/%%ADMIN_USERNAME%%/${ADMIN_USERNAME}/" /var/www/html/.env
-sed -i "s,%%ADMIN_PASSWORD%%,$HASHED_PASSWORD," /var/www/html/.env
+composer install
 
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 printf "${GREEN}Setup completed!${NC}"
-
-composer install
 
 exec tail -f /dev/null
