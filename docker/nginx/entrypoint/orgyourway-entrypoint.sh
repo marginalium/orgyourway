f3c4e47fae66fec3941c1c50caa337da90eca317
Dom DeStefano <domdest@gmail.com>
2022-10-26 01:01:06 -0400

    remove console list debug call from entrypoint.sh and pass environment variables directly into Doctrine migrate

--- docker/nginx/entrypoint/orgyourway-entrypoint.sh
+++ docker/nginx/entrypoint/orgyourway-entrypoint.sh
@@ -17,7 +17,7 @@ export MYSQL_PASSWORD=$MYSQL_PASSWORD
 export HASHED_PASSWORD=`php /entrypoint/bcrypt_password_hash.php`
 
 service nginx start
-
+service php8.1-fpm stop
 /etc/init.d/php8.1-fpm start
 
 if [ "$APP_ENV" = "dev" ]
@@ -25,8 +25,7 @@ then
 /entrypoint/waitforit.sh $MYSQL_HOST:3306 -t 100
 fi
 
-echo bin/console list
-
+MYSQL_DATABASE=$MYSQL_DATABASE MYSQL_HOST=$MYSQL_HOST MYSQL_UNIX_SOCKET=$MYSQL_UNIX_SOCKET MYSQL_USER=$MYSQL_USER MYSQL_PASSWORD=$MYSQL_PASSWORD \
 bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 composer install
