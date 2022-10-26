a1450255937b77f002f77ea3f1707edf75d06f41
domdest <domdest@gmail.com>
2022-10-26 19:46:55 -0400

    app_env prod by default

--- app/public/index.php
+++ app/public/index.php
@@ -6,7 +6,7 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     if (empty($context['APP_ENV'])) {
-        $context['APP_ENV'] = 'dev';
+        $context['APP_ENV'] = 'prod';
     }
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
