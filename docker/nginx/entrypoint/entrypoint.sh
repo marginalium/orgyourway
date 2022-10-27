c6d1cd7a40097880c71e4b58e4242e16335722da
domdest <domdest@gmail.com>
2022-10-26 23:35:21 -0400

    add file to build

--- docker/nginx/entrypoint/entrypoint.sh
+++ docker/nginx/entrypoint/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Starting entrypoint for ${ORG_ENV}...}";
-ls "/cloudsql/"
+echo "Starting entrypoint for ${ORG_ENV}...";
+ls /cloudsql/
 file "${MYSQL_UNIX_SOCKET}"
 sh /entrypoint/orgyourway-entrypoint-"${ORG_ENV}".sh
