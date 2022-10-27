fdd9b45bc99b4efed8644d91c8078607d4d59fc9
Dom DeStefano <domdest@gmail.com>
2022-10-27 14:24:34 -0400

    use ORG_ENV all the way through the stack until symfony

--- docker/nginx/entrypoint/entrypoint.sh
+++ docker/nginx/entrypoint/entrypoint.sh
@@ -11,7 +11,7 @@ service php8.1-fpm start
 
 export ADMIN_USERNAME=$ADMIN_USERNAME
 export ADMIN_PASSWORD=$ADMIN_PASSWORD
-export APP_ENV=$ORG_ENV
+export ORG_ENV=$ORG_ENV
 export APP_NAME=$APP_NAME
 export MYSQL_DATABASE=$MYSQL_DATABASE
 export MYSQL_HOST=$MYSQL_HOST
@@ -24,7 +24,7 @@ cp /var/www/html/.env.dist /var/www/html/.env
 
 sed -i "s,%%ADMIN_PASSWORD%%,${ADMIN_PASSWORD}," /var/www/html/.env
 sed -i "s/%%ADMIN_USERNAME%%/${ADMIN_USERNAME}/" /var/www/html/.env
-sed -i "s/%%APP_ENV%%/${APP_ENV}/" /var/www/html/.env
+sed -i "s/%%APP_ENV%%/${ORG_ENV}/" /var/www/html/.env
 sed -i "s/%%APP_NAME%%/${APP_NAME}/" /var/www/html/.env
 sed -i "s,%%HASHED_PASSWORD%%,${HASHED_PASSWORD}," /var/www/html/.env
 sed -i "s/%%MYSQL_HOST%%/${MYSQL_HOST}/" /var/www/html/.env
@@ -34,7 +34,7 @@ sed -i "s/%%MYSQL_DATABASE%%/${MYSQL_DATABASE}/" /var/www/html/.env
 sed -i "s,%%MYSQL_UNIX_SOCKET%%,${MYSQL_UNIX_SOCKET}," /var/www/html/.env
 
 
-if [ "$APP_ENV" = "dev" ]
+if [ "$ORG_ENV" = "dev" ]
 then
   /entrypoint/waitforit.sh $MYSQL_HOST:3306 -t 100
 fi
