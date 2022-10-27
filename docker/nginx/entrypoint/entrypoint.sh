c6c661df241a944fdb133adb0249895b5544bbf7
domdest <domdest@gmail.com>
2022-10-26 23:20:45 -0400

    output unix socket

--- docker/nginx/entrypoint/entrypoint.sh
+++ docker/nginx/entrypoint/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Starting entrypoint for ${ORG_ENV}...";
+echo "Starting entrypoint for ${ORG_ENV}... Unix socket is ${MYSQL_UNIX_SOCKET}";
 sh /entrypoint/orgyourway-entrypoint-"${ORG_ENV}".sh
