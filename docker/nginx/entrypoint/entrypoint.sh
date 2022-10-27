68c13b5f987e2af6ffdd5870b45133cef79fcdb5
domdest <domdest@gmail.com>
2022-10-26 23:25:48 -0400

    output file check for unix socket path

--- docker/nginx/entrypoint/entrypoint.sh
+++ docker/nginx/entrypoint/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "Starting entrypoint for ${ORG_ENV}... Unix socket is ${MYSQL_UNIX_SOCKET}";
+echo "Starting entrypoint for ${ORG_ENV}...}";
+ls "/cloudsql/"
+file "${MYSQL_UNIX_SOCKET}"
 sh /entrypoint/orgyourway-entrypoint-"${ORG_ENV}".sh
